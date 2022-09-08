b18bfbd2d69642c00a6fcaa4916724901fc52dc6
Ilya <ilya@sinyugin.ru>
2022-09-08 10:27:43 +0200

    1.3 done

--- part-1/courseinfo/src/App.js
+++ part-1/courseinfo/src/App.js
@@ -1,5 +1,3 @@
-import { propTypes } from "react-bootstrap/esm/Image";
-
 const Header = (props) => {
   return (
     <div>
@@ -38,25 +36,31 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
       <Content
-        p1={part1}
-        p2={part2}
-        p3={part3}
-        e1={exercises1}
-        e2={exercises2}
-        e3={exercises3}
+        p1={part1.name}
+        p2={part2.name}
+        p3={part3.name}
+        e1={part1.exercises}
+        e2={part2.exercises}
+        e3={part3.exercises}
       />
-      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises} />
     </div>
   );
 };
