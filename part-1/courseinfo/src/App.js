a655428d7d39a49c85d7798143a194585db0edca
Ilya <ilya@sinyugin.ru>
2022-09-08 10:29:57 +0200

    1.4 done

--- part-1/courseinfo/src/App.js
+++ part-1/courseinfo/src/App.js
@@ -36,31 +36,37 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
       <Content
-        p1={part1.name}
-        p2={part2.name}
-        p3={part3.name}
-        e1={part1.exercises}
-        e2={part2.exercises}
-        e3={part3.exercises}
+        p1={parts[0].name}
+        p2={parts[1].name}
+        p3={parts[2].name}
+        e1={parts[0].exercises}
+        e2={parts[1].exercises}
+        e3={parts[2].exercises}
       />
-      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises} />
+      <Total
+        e1={parts[0].exercises}
+        e2={parts[1].exercises}
+        e3={parts[2].exercises}
+      />
     </div>
   );
 };
