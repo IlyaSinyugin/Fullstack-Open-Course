ec86722ab5ac091f09a12fb9daae8404f54bb61a
Ilya <ilya@sinyugin.ru>
2022-09-09 09:19:10 +0200

    1.7

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -5,12 +5,18 @@ const Button = ({ handleClick, text }) => {
 };
 
 const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good + bad * -1) / all;
+  const positive = (good / all) * 100;
   return (
     <div>
       <h1>statistics</h1>
       <div>good: {good}</div>
       <div>neutral: {neutral}</div>
       <div>bad: {bad}</div>
+      <div>all: {all}</div>
+      <div>average: {average}</div>
+      <div>positive: {positive}%</div>
     </div>
   );
 };
