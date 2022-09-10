87e9d73d414c8964fe5774a8ee2748484271b708
Ilya <ilya@sinyugin.ru>
2022-09-10 10:33:30 +0200

    1.13

--- part-1/anecdotes/src/App.js
+++ part-1/anecdotes/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
+
 const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
@@ -15,15 +16,25 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
   const handleSelected = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
   };
 
+  const handleVotes = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
   return (
     <div>
+      {console.log(votes)}
       <p> {anecdotes[selected]}</p>
-      <Button handleClick={handleSelected} text="random anecdote" />
+      <Button handleClick={handleVotes} text="vote" />
+      <Button handleClick={handleSelected} text="next anecdote" />
+      <p>has {votes[selected]} votes</p>
     </div>
   );
 };
