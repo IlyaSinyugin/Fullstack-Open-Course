f5c6695c2a923501e6b640fafb45e4fdec536d29
Ilya <ilya@sinyugin.ru>
2022-09-15 10:13:31 +0200

    2.15 and 2.16

---
+++ part-2/phonebook/src/services/persons.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => {
+  return axios.get(baseUrl);
+};
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject);
+};
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject);
+};
+
+export default { getAll, create, update };
