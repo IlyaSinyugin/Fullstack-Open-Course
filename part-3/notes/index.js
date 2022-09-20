99346dcd632a1eaa63093b70cff0aff36e198876
Ilya <ilya@sinyugin.ru>
2022-09-20 10:14:56 +0200

    3.7

--- part-3/notes/index.js
+++ part-3/notes/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
 let notes = [
   {
     id: 1,
@@ -23,6 +21,18 @@ let notes = [
     important: true,
   },
 ];
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body: ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+
+app.use(requestLogger);
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -69,6 +79,12 @@ app.post("/api/notes", (request, response) => {
 
   response.json(note);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
