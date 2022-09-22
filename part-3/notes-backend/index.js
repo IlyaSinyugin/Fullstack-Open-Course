670d7904f02346eaec8d5fa77f0a1f20f4dc1627
Ilya <ilya@sinyugin.ru>
2022-09-22 14:28:44 +0200

    notes

--- part-3/notes-backend/index.js
+++ part-3/notes-backend/index.js
@@ -1,105 +1,129 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { response } = require("express");
+const password = process.env.PASSWORD;
+const Note = require("./models/note");
+const PORT = process.env.PORT;
+
+// const url = `mongodb+srv://fullstack:${password}@cluster0.beu5aww.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+// mongoose.connect(url);
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: Date,
+//   important: Boolean,
+// });
+
+// const Note = mongoose.model("Note", noteSchema);
+
+// noteSchema.set("toJSON", {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
 
 let notes = [
   {
     id: 1,
     content: "HTML is easy",
     date: "2022-01-10T17:30:31.098Z",
-    important: true
+    important: true,
   },
   {
     id: 2,
     content: "Browser can execute only Javascript",
     date: "2022-01-10T18:39:34.091Z",
-    important: false
+    important: false,
   },
   {
     id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+    important: true,
+  },
+];
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(requestLogger)
+app.use(requestLogger);
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
     id: generateId(),
-  }
+  });
 
-  notes = notes.concat(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
 
-  response.json(note)
-})
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+app.delete("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
 
   if (note) {
-    response.json(note)
+    response.json(note);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
