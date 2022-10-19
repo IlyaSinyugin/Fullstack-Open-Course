30c31fac0640c29ecbae05ed872c117e116e0801
Ilya <ilya@sinyugin.ru>
2022-10-19 17:33:40 +0200

    notes

--- part-3/notes-backend/models/note.js
+++ part-3/notes-backend/models/note.js
@@ -15,8 +15,15 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
+  content: {
+    type: String,
+    mingLength: 5,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
   important: Boolean,
 });
 
