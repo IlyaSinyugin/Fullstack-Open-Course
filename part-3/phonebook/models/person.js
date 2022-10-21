c8fe667e1213f09d0f38a39c10a984b080ee9d98
Ilya <ilya@sinyugin.ru>
2022-10-21 10:04:59 +0200

    3.20

--- part-3/phonebook/models/person.js
+++ part-3/phonebook/models/person.js
@@ -20,7 +20,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {
